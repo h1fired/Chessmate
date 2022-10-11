ae76242a18191b45ff036571455fff208c00f092
misha <misha@DESKTOP-195OTGU>
2022-10-11 20:30:52 +0300

    Рендер графіки
    Візуалізація, мультибуферизація, анти-аліасінг, тест графіки

--- chess/src/com/chessmate/display/Display.java
+++ chess/src/com/chessmate/display/Display.java
@@ -1,5 +1,94 @@
 package com.chessmate.display;
 
-public class Display {
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+import java.util.Arrays;
 
+import javax.swing.JFrame;
+
+public abstract class Display {
+	
+	private static boolean created = false; //Статус вікна гри (створений чи ні)
+	private static JFrame window;
+	private static Canvas content;
+	
+	private static BufferedImage buffer;
+	private static int[] bufferData;
+	private static Graphics bufferGraphics;
+	private static int clearColor;
+	
+	private static BufferStrategy bufferStrategy;
+	
+	//tmp
+	private static float delta = 0;
+	//tmp end
+	
+	public static void create(int width, int height, String title, int _clearColor, int numBuffers) {
+		//Перевірка чи запущена гра, якщо ні - виходить з функції
+		if(created) { 
+			return;
+		}
+		
+		//Створення вікна гри
+		window = new JFrame(title);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		content = new Canvas();
+		
+		Dimension size = new Dimension(width, height);
+		content.setPreferredSize(size);
+		
+		window.setResizable(false);
+		window.getContentPane().add(content);
+		window.pack();
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);
+		
+		//Буфер для графіки цілого вікна
+		buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		bufferData = ((DataBufferInt)buffer.getRaster().getDataBuffer()).getData();
+		bufferGraphics = buffer.getGraphics();
+		clearColor = _clearColor;
+		
+		//Мульти буферизація
+		content.createBufferStrategy(numBuffers);
+		bufferStrategy = content.getBufferStrategy();
+		
+		created = true;
+	}
+	
+	//Очищення буфера
+	public static void clear() {
+		Arrays.fill(bufferData, clearColor);
+	}
+	
+	//Рендер (візуалізація) графіки
+	public static void render() {
+		bufferGraphics.setColor(new Color(0xff0000ff));
+		bufferGraphics.fillOval((int)(1280 / 2 - 50 + (Math.sin(delta) * 200)), 720 / 2 - 50, 100, 100);
+		
+		((Graphics2D)bufferGraphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		
+		bufferGraphics.setColor(new Color(0xff0000ff));
+		bufferGraphics.fillOval((int)(1280 / 2 - 50 + (Math.sin(delta) * 200)), 720 / 2 - 50 + 130, 100, 100);
+		((Graphics2D)bufferGraphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
+		
+		delta += 0.02f;
+		
+		
+	}
+	
+	// Заміна стандартного буфера на створений buffer
+	public static void swapBuffers() {
+		Graphics g = bufferStrategy.getDrawGraphics();
+		g.drawImage(buffer, 0, 0, null);
+		bufferStrategy.show();
+	}
+	
 }
--- chess/src/com/chessmate/main/Main.java
+++ chess/src/com/chessmate/main/Main.java
@@ -1,10 +1,27 @@
 package com.chessmate.main;
 
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.Timer;
+
+import com.chessmate.display.Display;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Display.create(1280, 720, "Chessmate", 0xff000000, 3);
+		
+		Timer t = new Timer(1000 / 60, new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				Display.clear();
+				Display.render();
+				Display.swapBuffers();
+			}
+		});
+		
+		t.setRepeats(true);
+		t.start();
 	}
 
 }
